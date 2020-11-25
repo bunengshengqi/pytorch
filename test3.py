11683c1389ffa811c308e27da648708301afe691
奔跑的小鲁迪 <61943181@qq.com>
2020-11-25 21:17:40 +0800

    tensor的数学运算

---
+++ test3.py
@@ -0,0 +1,9 @@
+#  tensor的四则运算
+# 加法 add  减法 sub  乘法(哈达玛积) mul  除法div
+# 二维矩阵的运算   torch.mm()   torch.matmul()   @ 一共三种
+# 高维的tensor   只支持  torch.matmul()
+# tensor 的幂运算
+# torch.pow()
+# e的n次方  torch.exp()
+# 开方运算 sqrt()
+# 对数运算  torch.log2()   torch.log10()
