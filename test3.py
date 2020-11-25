517c2356633901a17eedbe158d68b98c1f56e778
奔跑的小鲁迪 <61943181@qq.com>
2020-11-25 21:24:11 +0800

    tensor的数学运算

--- test3.py
+++ test3.py
@@ -7,3 +7,20 @@
 # e的n次方  torch.exp()
 # 开方运算 sqrt()
 # 对数运算  torch.log2()   torch.log10()
+
+# add
+
+import torch
+a = torch.rand(2, 3)
+b = torch.rand(2, 3)
+print(a)
+print(b)
+
+# 加法的几种形式
+print(a+b)
+print(a.add(b))
+print(torch.add(a, b))
+print('------')
+print(a.add_(b))
+print('------')
+print(a)
